4bf7d360bd68b7fc71b842129d68e491fc9a1722
MukulDevPatel <patelmukuldev@gmail.com>
2023-07-10 08:42:34 +0530

    [Mukul Dev] Add: We write a code for find integer value and also inserted new int value and removed from the file

---
+++ DataStructureProgramming/FindInteger.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms.DataStructureProgramming
+{
+    public class FindInteger
+    {
+        public static void Execute()
+        {
+            LinkedList<int> numbersList = new LinkedList<int>();
+
+            // Read numbers from file and arrange in ascending order
+            string filePath = @"D:\BridgeLabz Second batch\AlgorithmPrograms\DataStructureProgramming\IntegerFile.txt";
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines)
+            {
+                int number = int.Parse(line);
+                InsertInOrder(numbersList, number);
+            }
+
+            Console.WriteLine("Enter a number:");
+            int userInput = int.Parse(Console.ReadLine());
+
+            // Check if number exists in the list
+            LinkedListNode<int> node = numbersList.Find(userInput);
+            if (node != null)
+            {
+                // Remove number from the list if found
+                numbersList.Remove(node);
+                Console.WriteLine("Number found and removed from the list.");
+            }
+            else
+            {
+                // Insert number in the appropriate position
+                InsertInOrder(numbersList, userInput);
+                Console.WriteLine("Number inserted in the appropriate position.");
+            }
+
+            // Write the updated list of numbers to file
+            WriteNumbersToFile(numbersList, filePath);
+
+            Console.WriteLine("Numbers written to file.");
+        }
+
+        static void InsertInOrder(LinkedList<int> list, int number)
+        {
+            if (list.Count == 0 || number < list.First.Value)
+            {
+                list.AddFirst(number);
+                return;
+            }
+
+            LinkedListNode<int> current = list.First;
+            while (current.Next != null && current.Next.Value < number)
+            {
+                current = current.Next;
+            }
+
+            list.AddAfter(current, number);
+        }
+
+        static void WriteNumbersToFile(LinkedList<int> list, string filePath)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                foreach (int number in list)
+                {
+                    writer.WriteLine(number);
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,39 +7,8 @@ class Program
     {
         Console.WriteLine("Welcome to Data Structure Programming");
 
-        Console.WriteLine("Search word from the file");
+        Console.WriteLine("Search number from the file");
 
-        LinkedList wordList = new LinkedList();
-        string fileName = "D:\\BridgeLabz Second batch\\AlgorithmPrograms\\DataStructureProgramming\\WordFile.txt";
-
-        // Read the text from a file and split it into words
-        //string[] text = File.ReadAllText(fileName).Split(' ');
-        string[] words = File.ReadAllText(fileName).Split(new char[] { ' ', '\t', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
-
-        // Create the linked list
-        foreach (string word in words)
-        {
-            wordList.AddWord(word);
-        }
-
-        // Take user input to search a word
-        Console.Write("Enter a word to search: ");
-        string searchWord = Console.ReadLine();
-
-        // Search the word in the list and perform necessary operations
-        if (wordList.SearchWord(searchWord))
-        {
-            Console.Write("Word found in the list: ");
-            wordList.RemoveWord(searchWord);
-        }
-        else
-        {
-            Console.Write("Word not found in the list: ");
-            wordList.AddWord(searchWord);
-        }
-
-        // Save the list into a file
-        wordList.SaveToFile(fileName);
-        wordList.Print();
+        FindInteger.Execute();
     }
 }
